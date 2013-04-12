43781e1f1cab90d0cf109dc3c4aeb1253fe365c6
realworld666 <realworld666@gmail.com>
2013-04-12 13:07:18 +0000

    Added cross hair to directions screen Add waypoint lays down the start wisp at the current selected position

--- Cyclestreets/Directions.xaml.cs
+++ Cyclestreets/Directions.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Microsoft.Phone.Maps.Services;
 using System.Device.Location;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Services;
+using Microsoft.Phone.Maps.Toolkit;
 
 namespace Cyclestreets
 {
@@ -16,6 +13,9 @@ namespace Cyclestreets
 	{
 		ReverseGeocodeQuery geoQ = null;
 		List<GeoCoordinate> waypoints = new List<GeoCoordinate>();
+		MapLayer wayPointLayer = null;
+
+		GeoCoordinate current = null;
 
 		public Directions()
 		{
@@ -25,22 +25,55 @@ namespace Cyclestreets
 			geoQ.QueryCompleted += geoQ_QueryCompleted;
 		}
 
-		private void geoQ_QueryCompleted(object sender, QueryCompletedEventArgs<IList<MapLocation>> e)
+		private void geoQ_QueryCompleted( object sender, QueryCompletedEventArgs<IList<MapLocation>> e )
 		{
 			MapLocation loc = e.Result[0];
 			startPoint.Text = loc.Information.Address.Street + ", " + loc.Information.Address.City + ", " + loc.Information.Address.PostalCode;
 		}
 
-		private void myPosition_Click(object sender, EventArgs e)
+		private void startPoint_SelectionChanged( object sender, System.Windows.Controls.SelectionChangedEventArgs e )
 		{
-			geoQ.GeoCoordinate = CoordinateConverter.ConvertGeocoordinate(MainPage.MyGeoPosition.Coordinate);
+			//start = startPoint.SelectedItem as SearchResult;
+		}
+
+		private void myPosition_Click( object sender, EventArgs e )
+		{
+			geoQ.GeoCoordinate = CoordinateConverter.ConvertGeocoordinate( MainPage.MyGeoPosition.Coordinate );
 			geoQ.QueryAsync();
 
-			SmartDispatcher.BeginInvoke(() =>
+			current = geoQ.GeoCoordinate;
+
+			SmartDispatcher.BeginInvoke( () =>
 			{
-				MyMap.SetView(geoQ.GeoCoordinate, 16);
+				MyMap.SetView( geoQ.GeoCoordinate, 16 );
 				//MyMap.Center = CoordinateConverter.ConvertGeocoordinate(MyGeoPosition.Coordinate);
-			});
+			} );
+		}
+
+		private void confirmWaypoint_Click( object sender, EventArgs e )
+		{
+			if( wayPointLayer == null )
+			{
+				wayPointLayer = new MapLayer();
+
+				MyMap.Layers.Add( wayPointLayer );
+			}
+
+			Pushpin pp = new Pushpin();
+			pp.Style = Resources["Start"] as Style;
+			MapOverlay overlay = new MapOverlay();
+			overlay.Content = pp;
+			overlay.GeoCoordinate = current;
+			overlay.PositionOrigin = new Point( 0.3, 1.0 );
+			wayPointLayer.Add( overlay );
+			/*BitmapImage imgSource = new BitmapImage( new Uri( "/Assets/start_wisp.png", UriKind.Relative ) );
+			//BitmapImage imgSource = new BitmapImage( new Uri( "/Assets/start_wisp.png", UriKind.RelativeOrAbsolute ) );
+			MapOverlay overlay = new MapOverlay();
+			overlay.Content = imgSource;
+			overlay.GeoCoordinate = current;
+			overlay.PositionOrigin = new Point( 0.5, 1.0 );
+			wayPointLayer.Add( overlay );*/
+
 		}
 	}
 }
