58148105ff377a522ce12574efee1c04bbb95091
Dave Conley <dave.conley@live.co.uk>
2014-07-16 18:15:49 +0100

    added converters

---
+++ Common/BooleanToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ScoreAlerts.Common
+{
+    /// <summary>
+    /// Value converter that translates true to <see cref="Visibility.Visible"/> and false to
+    /// <see cref="Visibility.Collapsed"/>.
+    /// </summary>
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+
+    public sealed class BooleanInvertedToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool && !(bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
